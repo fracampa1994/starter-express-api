7dae659cabea09e139fc211c822042768ed38119
kampa94 <francescocampagnaro1994@gmail.com>
2024-01-17 14:14:32 +0100

    commit
    Signed-off-by: kampa94 <francescocampagnaro1994@gmail.com>

--- index.ts
+++ index.ts
@@ -31,7 +31,8 @@ app.get('/:timestamp/track.png', (req: Request, res: Response) => {
   pusher.trigger('EmailTracker', 'email-read', {
     "emailSentDate": new Date().toLocaleString(),
     "userAgent": req.headers['user-agent'],
-    "ipAddr": req.socket.remoteAddress
+    "ipAddr": req.header('x-forwarded-for') || req.socket.remoteAddress;
+
   });
 
 
